5396bfaf027bac6b4605ca07edf4a7d2828c8d63
SanDiego <sandiegocastilho@gmail.com>
2017-11-30 13:45:18 -0300

    implementacao das funcoes: acessa_v, atribui_v, atribui_direita, atribui_esquerda, acessa_direita_ acessa_esquerda e tamanho_v

--- Rede Social/Viagem.c
+++ Rede Social/Viagem.c
@@ -12,11 +12,32 @@ typedef struct viagem{
 
 } Viagem;
 
-//Cira uma nova viagem
+//Cria uma nova viagem.
 Viagem *nova_v(int dia, int mes, int ano, char *cidade, char *pais, int periodo);
 
-//Libera uma viagem
+//Libera uma viagem.
 void libera_v(Viagem *viagem);
+
+//Acessa os dados de determinada viagem.
+void acessa_v(Viagem *viagem, int *dia, int *mes, int *ano, char *cidade, char *pais, int *periodo);
+
+//Atribui novos valores a uma dada viagem.
+void atribui_v(Viagem *viagem, int dia, int mes, int ano, char *ciadade, char *pais, int periodo);
+
+//Atribui uma viagem à direita da viagem dada;
+void atribui_direita_v(Viagem *viagem, Viagem *direita);
+
+//Acessa a Viagem a direita da Viagem dada.
+Viagem *acessa_direita_v(Viagem *viagem);
+
+//Atribui uma Viagem à esquerda da viagem dada.
+void atribui_esquerda_v(Viagem *viagem, Viagem *esquerda);
+
+//Acessa a Viagem a esquerda da Viagem dada.
+Viagem *acessa_esquerda_v(Viagem *viagem);
+
+//Retorna o tamanho da estrutura Viagem.
+int tamanho_v();
 
 int VerificaCidade(char *cidade){
 	int Resultado = (cidade == NULL || strlen(cidade) > 60) ? 0 : 1;
@@ -66,4 +87,62 @@ void libera_v(Viagem *viagem){
 		viagem->direita = NULL;
 		viagem->esquerda = NULL;
 	}
+}
+
+void acessa_v(Viagem *viagem, int *dia, int *mes, int *ano, char *cidade, char *pais, int *periodo){
+	if (viagem != NULL){
+		*dia = viagem->dia;
+		*mes = viagem->mes;
+		*ano = viagem->ano;
+		*periodo = viagem->periodo;
+		strcpy(pais, viagem->pais);
+		strcpy(cidade, viagem->cidade);
+	}
+}
+
+void atribui_v(Viagem *viagem, int dia, int mes, int ano, char *cidade, char *pais, int periodo){
+	if (viagem != NULL){
+		viagem->dia = dia;
+		viagem->mes = mes;
+		viagem->ano = ano;
+		viagem->periodo = periodo;
+		strcpy(viagem->cidade, cidade);
+		strcpy(viagem->pais, pais);
+	}
+}
+
+void atribui_direita_v(Viagem *viagem, Viagem *direita){
+	if (viagem != NULL){
+		viagem->direita = direita;
+	}
+}
+
+Viagem *acessa_direita_v(Viagem *viagem){
+	if (viagem != NULL){
+		Viagem *pont = viagem->direita;
+	
+		return pont;
+	}
+}
+
+void atribui_esquerda_v(Viagem *viagem, Viagem *esquerda){
+	if (viagem != NULL){
+		viagem->esquerda = esquerda;
+	}
+}
+
+Viagem *acessa_esquerda_v(Viagem *viagem){
+	if(viagem != NULL){
+		Viagem *pont = viagem->esquerda;
+
+		return pont;
+	}
+}
+
+int tamanho(){
+	int tam;
+
+	tam = sizeof(Viagem);
+
+	return tam;
 }
